77c545a7d4fa5f9ed270d6bbb856bc4e5604a245
Chunchun Wu <cw4281@nyu.edu>
2024-04-02 23:07:36 -0400

    update

--- Lab-subnet/subnet.py
+++ Lab-subnet/subnet.py
@@ -2,64 +2,64 @@
 uniquecode = 54272
 
 #problem 1
-p1Network = '0.226.48.192'
-p1Broadcast = '0.226.48.255'
-p1First = '0.226.48.193'
-p1Last =  '0.226.48.254'
+p1Network = '0.226.48.0'
+p1Broadcast = '0.226.48.63'
+p1First = '0.226.48.1'
+p1Last =  '0.226.48.62'
 
 #problem 2
-p2Network = ''
-p2Broadcast = ''
-p2First = ''
-p2Last = ''
+p2Network = '167.211.9.0'
+p2Broadcast = '167.211.9.255'
+p2First = '167.211.9.1'
+p2Last = '167.211.9.254'
 
 #problem 3
-p3Network = ''
-p3Broadcast = ''
-p3First = ''
-p3Last = ''
+p3Network = '78.197.227.128'
+p3Broadcast = '78.197.227.159'
+p3First = '78.197.227.129'
+p3Last = '78.197.227.158'
 
 #problem 4
-p4Network = ''
-p4Broadcast = ''
-p4First = ''
-p4Last = ''
+p4Network = '245.182.189.128'
+p4Broadcast = '245.182.189.255'
+p4First = '245.182.189.129'
+p4Last = '245.182.189.254'
 
 #problem 5
-p5Network = ''
-p5Broadcast = ''
-p5First = ''
-p5Last = ''
+p5Network = '156.167.151.224'
+p5Broadcast = '156.167.151.239'
+p5First = '156.167.151.225'
+p5Last = '156.167.151.238'
 
 #problem 6
-p6Network = ''
-p6Broadcast = ''
-p6First = ''
-p6Last =  ''
+p6Network = '67.152.112.0'
+p6Broadcast = '67.152.112.127'
+p6First = '67.152.112.1'
+p6Last =  '67.152.112.126'
 
 #problem 7
-p7Network = ''
-p7Broadcast = ''
-p7First = ''
-p7Last = ''
+p7Network = '234.137.74.56'
+p7Broadcast = '234.137.74.63'
+p7First = '234.137.74.57'
+p7Last = '234.137.74.62'
 
 #problem 8
-p8Network = ''
-p8Broadcast = ''
-p8First = ''
-p8Last = ''
+p8Network = '145.122.36.64'
+p8Broadcast = '145.122.36.127'
+p8First = '145.122.36.65'
+p8Last = '145.122.36.126'
 
 #problem 9
-p9Network = ''
-p9Broadcast = ''
-p9First = ''
-p9Last = ''
+p9Network = '56.107.254.0'
+p9Broadcast = '56.107.254.255'
+p9First = '56.107.254.1'
+p9Last = '56.107.254.254'
 
 #problem 10
-p10Network = ''
-p10Broadcast = ''
-p10First = ''
-p10Last = ''
+p10Network = '223.93.216.160'
+p10Broadcast = '223.93.216.191'
+p10First = '223.93.216.161'
+p10Last = '223.93.216.190'
 
 
 """
